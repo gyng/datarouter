b1e6b39f6ef867f7362a6e41884b0ce402efee5f
Ng Guoyou <ng.guoyou@gmail.com>
2017-07-10 17:04:40 +0800

    Initial working commit: routes logs from HTTP to stdout

---
+++ README.md
@@ -0,0 +1,9 @@
+# <Untitled project>
+
+In development. Intended to route logs/data from one place to another, with processing as needed. Maybe a simpler version of rsyslog?
+
+## TODO 
+
+* Fanout
+* Postgres output
+* Configuration (declarative)
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 extern crate rocket;
 
+use std::fmt::Debug;
+
 #[derive(Debug)]
 struct Log {
     timestamp: u32,
@@ -16,71 +18,98 @@ impl Log {
     }
 }
 
-trait Node {
-    fn start(&self) -> Result<(), String>;
-    fn is_input(&self) -> bool;
-    fn chain(&self, node: Box<Node>) -> Result<(), String>;
-    fn stop(&self) -> Result<(), String>;
-    // Should encapsulate chaining logic?
-    // fn send(&self, data: Log) -> 
-    fn process(&self, data: Log) -> Result<(), String> {
-        println!("{:?}", data);
-        Ok(())
-    }
-    // fn chained(&self) -> Vec<Box<Node>>;
-    // fn forward(&self) -> Result<(), String>;
-}
-
-struct HttpInputNode {
-    chained: Vec<Box<Node>>,
-    rocket: Option<rocket::Rocket>,
-}
-
-mod HttpInput {
-    #[get("/")]
-    fn index() -> &'static str {
-        "Hello, world!"
-    }
-
-    // replace with post, check jwt
-    #[get("/logs/<label>", format="application/json")]
-    fn logs(label: &str) {
-        ????process(super::Log::new(label.to_string(), Some("HELLO".to_string())))
-    }
-}
-
-impl HttpInputNode {
-    fn new(config: &String) -> HttpInputNode {
-        HttpInputNode { chained: vec!(), rocket: None }
-    }
-}
-
-impl Node for HttpInputNode {
-    fn start(&self) -> Result<(), String> {
-        rocket::ignite()
-            .manage(self)
-            .mount("/", routes![HttpInput::index, HttpInput::logs]).launch();
-        Ok(())
-    }
-
-    fn is_input(&self) -> bool {
-        true
-    }
-
-    fn chain(&self, node: Box<Node>) -> Result<(), String> {
+trait Node: Send + Sync + Debug {
+    fn start(self) -> Result<(), String> where Self: std::marker::Sized {
         Ok(())
     }
 
     fn stop(&self) -> Result<(), String> {
         Ok(())
     }
+
+    fn process(&self, data: Log) -> Result<(), String> {
+        // passthrough
+        self.process_next(data)
+    }
+
+    fn process_next(&self, data: Log) -> Result<(), String> {
+        if let &Some(ref next) = self.get_next() {
+            return next.process(data);
+        }
+
+        Ok(())
+    }
+
+    fn get_next(&self) -> &Option<Box<Node>>;
 }
 
-struct PostgresOutputNode {
-    chained: Vec<Box<Node>>,
+#[derive(Debug)]
+struct HttpInputNode {
+    next: Option<Box<Node>>,
+}
+
+mod http_input_node {
+    use rocket::State;
+    use super::{Log, Node};
+
+    #[get("/")]
+    fn index() -> &'static str {
+        "Hello, world!"
+    }
+
+    #[get("/logs/<label>")]
+    fn logs(label: &str, node: State<Box<Node>>) {
+        println!("{:?}", node);
+        let _ = node.process(Log::new("lol".to_string(), Some(label.to_string())));
+    }
+}
+
+impl HttpInputNode {
+    fn new(config: &String, next: Option<Box<Node>>) -> Self {
+        Self { next: next }
+    }
+}
+
+impl Node for HttpInputNode {
+    fn start(self) -> Result<(), String> {
+        let node: Box<Node> = Box::new(self);
+
+        rocket::ignite()
+            .manage(node)
+            .mount("/", routes![http_input_node::index, http_input_node::logs]).launch();
+
+        Ok(())
+    }
+
+    fn get_next(&self) -> &Option<Box<Node>> {
+        &self.next
+    }
+}
+
+#[derive(Debug)]
+struct StdoutOutputNode {
+    next: Option<Box<Node>>,
+}
+
+impl StdoutOutputNode {
+    fn new(config: &String, next: Option<Box<Node>>) -> Self {
+        Self { next: next }
+    }
+}
+
+impl Node for StdoutOutputNode {
+    fn get_next(&self) -> &Option<Box<Node>> {
+        &self.next
+    }
+
+    fn process(&self, data: Log) -> Result<(), String> {
+        println!("{:?}", data);
+        self.process_next(data)
+    }
 }
 
 fn main() {
-    let rooter = HttpInputNode::new(&"".to_string());
-    rooter.start();
+    let _ = HttpInputNode::new(&"".to_string(),
+        Some(Box::new(StdoutOutputNode::new(&"".to_string(), None)))
+    ).start();
 }
